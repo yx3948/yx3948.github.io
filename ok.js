fda18efa9702a5bb115396c22afacac5c20f2bc0
evelynxie <yx3948@nyu.edu>
2026-03-08 01:38:20 -0500

    week7update

--- ok.js
+++ ok.js
@@ -2,7 +2,8 @@
     
     let eggElement = document.getElementById("egg");
 
-    let wINdo = ["vomiting leaf", "wandering trill","good dull filler","really thick curve", "winnowy gullet", "random health","full belly","orange","loud keyboard","calculaitng turn","burgeoning chest","zinger"]
+    let wINdo = ["vomiting leaf", "wandering trill","good dull filler","really thick curve", "winnowy gullet", 
+        "random health","full belly","orange","loud keyboard","calculaitng turn","burgeoning chest","zinger"]
        
     
     document.getElementById("lorem").onclick = function(){
@@ -14,7 +15,7 @@
         let chosenColor = colorList[randomIndex];
         document.body.style.backgroundColor = chosenColor;
     }
-        let fontSizes = ["15px", "30px", "60px"];
+        let fontSizes = ["15px","20px", "30px", "60px"];
     let tomatoBtn = document.getElementById("tomato");
     let loremBox = document.getElementById("lorem");
 
